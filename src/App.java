32c9119ed703a8abe304cd8b2c122b022a1392e9
davidbs09 <davidbissacodasilva@gmail.com>
2025-06-20 23:27:15 -0300

    save

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int option;
+        String marchas = "";
 
         System.out.println("Digite o modelo do carro:");
         String modelo = scan.nextLine();
@@ -23,64 +24,69 @@ public class App {
         boolean sair = false;
         while (!sair) {
             System.out.println("Funções do carro: \n");
-            System.out.println("1. Acelerar");
-            System.out.println("2. Frear");
+            System.out.println("1. Ligar o Carro");
+            System.out.println("2. Desligar Carro");
             System.out.println("3. Trocar marcha");
-            System.out.println("4. Virar para a direita");
-            System.out.println("5. Virar para a esquerda");
-            System.out.println("6. Sair do programa\n");
+            System.out.println("4. Acelerar");
+            System.out.println("5. Frear");
+            System.out.println("6. Verificar a velocidade");
+            System.out.println("7. Virar para a direita");
+            System.out.println("8. Virar para a esquerda");
+            System.out.println("9. Sair do programa\n");
             option = scan.nextInt();
 
             switch (option) {
                 case 1:
-                    System.out.println("O carro está acelerando!");
-                    break;
+                    if (carro.verificarLigado(true)) {
+                        System.out.println("\nO carro já está ligado.\n");
+                    } else {
+                        System.out.println("\nCarro agora está ligado.");
+                        break;
+                    }
                 case 2:
-                    System.out.println("O carro está freando!");
-                    break;
+                    if (carro.verificarLigado(false)) {
+                        System.out.println("\nO carro já está desligado.\n");
+                    } else {
+                        System.out.println("\nCarro agora está desligado");
+                        break;
+                    }
                 case 3:
-                    System.out.println("Você trocou a marcha do carro!");
+                    System.out.println("\nPainel de Marchas: \n");
+                    System.out.println("1. NEUTRO");
+                    System.out.println("2. PRIMEIRA");
+                    System.out.println("3. SEGUNDA");
+                    System.out.println("4. TERCEIRA");
+                    System.out.println("5. QUARTA");
+                    System.out.println("6. QUINTA");
+                    System.out.println("7. SEXTA");
+                    System.out.println("8. RE");
+                    System.out.println("9 . Voltar ao menu principal\n");
+                    marchas = scan.next().toUpperCase();
+                    if (marchas == "RE") {
+                        System.out.println("Marcha ré engatada");
+                    } else if (marchas == "NEUTRO") {
+                        System.out.println("Ponto morto engatado");
+                    } else if (marchas.equals("PRIMEIRA")) {
+                        System.out.println("Marcha " + marchas + " engatada. Pode acelerar se quiser :)");
+                    } else {
+                        System.out.println("\nMarcha inválida. Por favor, selecione uma marcha válida para acelerar.");
+                    }
                     break;
                 case 4:
-                    System.out.println("Você virou o carro para a direita!");
-                    break;
+                    if (marchas.equals("PRIMEIRA")) {
+                        
+                    }
                 case 5:
-                    System.out.println("Você virou o carro para a esquerda!");
+                    System.out.println("\nVocê virou o carro para a esquerda!");
                     break;
                 case 6:
-                    System.out.println("Saindo do programa...");
+                    System.out.println("\nSaindo do programa...");
                     sair = true;
                     break;
-                default:
-                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
             }
-
-            /*
-             * if (option == 3) {
-             * System.out.println("\n Painel de Marchas: \n");
-             * System.out.println("1. NEUTRO");
-             * System.out.println("2. PRIMEIRA");
-             * System.out.println("3. SEGUNDA");
-             * System.out.println("4. TERCEIRA");
-             * System.out.println("5. QUARTA");
-             * System.out.println("6. QUINTA");
-             * System.out.println("7. SEXTA");
-             * System.out.println("8. RE");
-             * System.out.println("9 . Voltar ao menu principal\n");
-             * int marchaEscolhida = scan.nextInt();
-             * Carro.marchas[] marchaSelecionada = Carro.marchas.values();
-             * if (marchaEscolhida >= 0 && marchaEscolhida < marchaSelecionada.length) {
-             * System.out.println("Você escolheu a marcha: " +
-             * marchaSelecionada[marchaEscolhida - 1]);
-             * } else if (marchaEscolhida == 9) {
-             * System.out.println("Voltando ao menu principal.");
-             * } else {
-             * System.out.println("Marcha inválida. Por favor, escolha uma marcha válida.");
-             * }
-             * }
-             * }
-             */
         }
         scan.close();
     }
 }
+// guardando método para depois
+// if (marchas == "NEUTRO" || carro.verificarLigado(false)) {}
--- src/Carro.java
+++ src/Carro.java
@@ -3,18 +3,21 @@ public class Carro {
     private String modelo;
     private String cor;
     private int ano;
-    private marchas marchaAtual = marchas.NEUTRO;
 
     public enum marchas {
         NEUTRO, PRIMEIRA, SEGUNDA, TERCEIRA, QUARTA, QUINTA, SEXTA, RE
     }
 
-    public void setMarchaAtual(marchas novaMarcha) {
-        this.marchaAtual = novaMarcha;
-    }
-    
-    public marchas getMarchaAtual() {
-        return marchaAtual;
+    public boolean verificarLigado(boolean ligado) {
+        ligado = false;
+        if(ligado == true) {
+            System.out.println("O carro está ligado.\n");
+        } else if(ligado == false){
+            System.out.println("O carro está desligado.\n");
+        } else {
+            System.out.println("Algo de errado.");
+        }
+        return ligado;
     }
 
     public Carro(String modelo, String cor, int ano) {
